1c3f2502a3d55490c984e0aee45bb15ddcc4b249
JozielSilva <Joziel2012Silva@gmail.com>
2021-05-03 10:32:42 -0300

    Damage on touch area

---
+++ Assets/Scripts/DamageOnTouch.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageOnTouch : MonoBehaviour
+{
+
+    public int Damage = 1;
+
+    public LayerMask Damageable;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (UnityExtensions.Contains(Damageable, other.gameObject.layer))
+        {
+
+            Health _health = other.GetComponent<Health>();
+
+            _health.TakeDamage(Damage);
+
+        }
+    }
+}
