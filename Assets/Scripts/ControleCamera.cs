5cb0e9fb6dc73f179858bc253e51a0c8d7123916
JozielSilva <Joziel2012Silva@gmail.com>
2021-05-23 13:39:36 -0300

    Ajust Camera controller

--- Assets/Scripts/ControleCamera.cs
+++ Assets/Scripts/ControleCamera.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 public class ControleCamera : MonoBehaviour
 {
@@ -8,8 +9,12 @@ public class ControleCamera : MonoBehaviour
     private Vector3 posanterior;
     public Transform alvo;
     public float camdistancia = -40;
-     
-   
+
+    [Title("Rotation Axis")]
+    public bool moveAxisX = true;
+    public float AngleX = 180;
+    public bool moveAxisY = true;
+    public float AngleY = 180;
 
     // Update is called once per frame
     void Update()
@@ -22,12 +27,35 @@ public class ControleCamera : MonoBehaviour
         {
             Vector3 direcao = posanterior - cam.ScreenToViewportPoint(Input.mousePosition);
             cam.transform.position = alvo.position;
-            cam.transform.Rotate(new Vector3(1, 0, 0), direcao.y * 180);
-            cam.transform.Rotate(new Vector3(0, 1, 0), -direcao.x * 180, Space.World);
+
+            if (moveAxisY)
+                cam.transform.Rotate(new Vector3(1, 0, 0), direcao.y * AngleY);
+            else
+                cam.transform.eulerAngles = new Vector3(AngleY, cam.transform.eulerAngles.y, cam.transform.eulerAngles.z);
+
+            if (moveAxisX)
+                cam.transform.Rotate(new Vector3(0, 1, 0), -direcao.x * AngleX, Space.World);
+            else
+                cam.transform.eulerAngles = new Vector3(cam.transform.eulerAngles.x, AngleX, cam.transform.eulerAngles.z);
+
             cam.transform.Translate(new Vector3(0, 0, camdistancia));
             posanterior = cam.ScreenToViewportPoint(Input.mousePosition);
 
-            
+
         }
+
+    }
+
+    private void LateUpdate()
+    {
+        cam.transform.position = alvo.position;
+        cam.transform.Translate(new Vector3(0, 0, camdistancia));
+
+
+        if (!moveAxisY)
+            cam.transform.eulerAngles = new Vector3(AngleY, cam.transform.eulerAngles.y, cam.transform.eulerAngles.z);
+
+        if (!moveAxisX)
+            cam.transform.eulerAngles = new Vector3(cam.transform.eulerAngles.x, AngleX, cam.transform.eulerAngles.z);
     }
 }
